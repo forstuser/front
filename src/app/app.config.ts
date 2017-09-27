166abb880dcc9261a2852236fd8e284783996054
cpugirl <pritam.kumar@binbill.com>
2017-09-27 17:06:52 +0530

    bug fix 3.1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 export const appConfig = {
       // apiUrl: 'http://localhost:3000/'
-      // apiUrl: 'https://admin-dev.binbill.com/',
-      apiUrl: 'https://admin.binbill.com/',
+      apiUrl: 'https://admin-dev.binbill.com/',
+      // apiUrl: 'https://admin.binbill.com/',
       // apiUrl: 'http://13.126.168.35:3000/',
-      // apiUrl: ' http://6091265f.ngrok.io/',
-      // imageUrl :'https://consumer-dev.binbill.com/'
-      imageUrl :'https://consumer.binbill.com/'
+      // apiUrl: ' http://7baaa0ba.ngrok.io/',
+      imageUrl :'https://consumer-dev.binbill.com/'
+      // imageUrl :'https://consumer.binbill.com/'
 };
