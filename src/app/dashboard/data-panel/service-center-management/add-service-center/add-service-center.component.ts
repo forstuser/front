448a89d3627082087cbcd6741166e2ded88e2e14
shobhit.karnatak <shobhit.karnatak@binbill.com>
2017-11-07 16:11:35 +0530

    Add

--- src/app/dashboard/data-panel/service-center-management/add-service-center/add-service-center.component.ts
+++ src/app/dashboard/data-panel/service-center-management/add-service-center/add-service-center.component.ts
@@ -11,60 +11,71 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add-service-center.component.html',
   styleUrls: ['./add-service-center.component.css']
 })
+
+
 export class AddServiceCenterComponent implements OnInit {
   public addserviceCenterForm: FormGroup;
   items: OfflineSeller;
   brands: Brand;
   cat:Category;
+
   constructor(private userService: UserService, private fb: FormBuilder, private functionService:FunctionService) {
-   }
+  }
+
   ngOnInit() {
+
       // get list of category
       this.userService.getCategoryList(2) // 2 for category refer to api doc
       .subscribe(getCat => {
         this.cat = getCat;
         console.log('category is ' + getCat);
       });
+
+
     this.userService.getBrandList()
       .subscribe( brandList => {
+        console.log(brandList,"brandlists")
         this.brands = brandList;
         console.log(this.brands);
       });
 
+      
     this.addserviceCenterForm = this.fb.group({
-      'Name' : ['', Validators.required],
-      'BrandID' : ['', Validators.required],
-      'HouseNo' : '',
-      'Block': '',
-      'Street': '',
-      'Sector': '',
-      'City': ['', Validators.required],
-      'State': ['', Validators.required],
-      'PinCode': '',
-      'NearBy': '',
-      'Lattitude': '',
-      'Longitude': '',
-      'OpenDays': ['', Validators.required],
-      'Timings': ['', Validators.required],
+      'center_name' : ['', Validators.required],
+      'center_brands' : ['', Validators.required],
+      'center_city': ['', Validators.required],
+      'center_state': ['', Validators.required],
+      'center_pin': '',
+      'center_country': ['',Validators.required],
+      'center_address': ['',Validators.required],      
+      'center_lattitude': '',
+      'center_longitude': '',
+      'center_days': ['', Validators.required],
+      'center_timings': ['', Validators.required],
       Details: this.fb.array([ this.createItem(), ])
     });
   }
+
+
   createItem() {
     return this.fb.group({
       'CategoryID':'',
       'DetailTypeID': '',
       'DisplayName': '',
-      'Details': ''
+      'Detail': ''
     });
   }
+
   addItem() {
     const control = <FormArray>this.addserviceCenterForm.controls['Details'];
     control.push(this.createItem());
   }
+
   removeDetails(i: number) {
     const control = <FormArray>this.addserviceCenterForm.controls['Details'];
     control.removeAt(i);
   }
+
   createOfflineSeller(data: OfflineSeller) {
     this.userService.createAuthorizedServiceCenter(data)
       .subscribe(res => {
@@ -73,9 +84,11 @@ export class AddServiceCenterComponent implements OnInit {
         this.addserviceCenterForm.reset();
       });
   }
+
   // function for avoid only space submit
   avoidSpace(e){
     console.log(e);
     this.functionService.NoWhitespaceValidator(this.addserviceCenterForm,e)
   }
+
 }
