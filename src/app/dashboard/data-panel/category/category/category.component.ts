abed11af25fad848910b9a73959378f65685c0af
shobhit.karnatak <shobhit.karnatak@binbill.com>
2017-11-09 20:19:52 +0530

    Add left panel

--- src/app/dashboard/data-panel/category/category/category.component.ts
+++ src/app/dashboard/data-panel/category/category/category.component.ts
@@ -124,6 +124,8 @@ export class CategoryComponent implements OnInit {
     //   category_id: catId
     // });
   }
+
+
   // after select main category show list of category
   onSelectMainCat(catID: number) {
     // console.log(this.createCategoryForm.value);
@@ -134,6 +136,8 @@ export class CategoryComponent implements OnInit {
         console.log(res, "category");
       });
   }
+
+  
   // after select category show  category form
   onSelectCat(catID: number) {
     this.showEdit = true;
@@ -144,6 +148,7 @@ export class CategoryComponent implements OnInit {
         console.log(this.catForm, "category form");
       });
   }
+
 
   // create category
   createCategory(category: any) {
