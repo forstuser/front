157aa250db7d5ceb2ccc0b4bd4c4fdab929deeb1
pritam parker <pritam.kumar@binbill.com>
2018-02-05 14:34:40 +0530

    new bug fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,6 @@
 export const appConfig = {
-      apiUrl: 'https://admin-stage.binbill.com/',
-      // apiUrl: 'https://admin-prod.binbill.com/',
-      // apiUrl: 'https://admin.binbill.com/',
-      // apiUrl: 'https://192.168.0.66:8080/',
-      // apiUrl: ' http://de759f5d.ngrok.io/',
+      // apiUrl: 'https://admin-stage.binbill.com/',
+      apiUrl: 'https://admin.binbill.com/',
       // imageUrl :'https://consumer-dev.binbill.com/'
       imageUrl: 'https://consumer.binbill.com/'
 };
