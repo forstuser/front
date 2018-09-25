59f0715ff4ba567cd892fec296d6e036155e5aec
cpugirl <pritam.kumar@binbill.com>
2018-09-25 20:06:05 +0530

    online order icon for all sections

--- src/app/dashboard/data-panel/jobs/cashback/process/skew/skew.component.ts
+++ src/app/dashboard/data-panel/jobs/cashback/process/skew/skew.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { UserService } from '../../../../../../_services/user.service';
-import { NgxNotificationService } from 'ngx-notification';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UserService } from "../../../../../../_services/user.service";
+import { NgxNotificationService } from "ngx-notification";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-skew',
-  templateUrl: './skew.component.html',
-  styleUrls: ['./skew.component.css']
+  selector: "app-skew",
+  templateUrl: "./skew.component.html",
+  styleUrls: ["./skew.component.css"]
 })
 export class SkewComponent implements OnInit {
-  @Input() jobDetails: any;
+  @Input()
+  jobDetails: any;
   skus: any;
   skuMeasurement: any;
   skuData: any = [];
@@ -18,10 +19,13 @@ export class SkewComponent implements OnInit {
   skuIds: any = [];
   sellerId: number;
   jobId: number;
-  constructor(private __router: Router, private __userService: UserService, private __ngxNotificationService: NgxNotificationService) { }
+  constructor(
+    private __router: Router,
+    private __userService: UserService,
+    private __ngxNotificationService: NgxNotificationService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   ngOnChanges() {
     // console.log("from parent", this.jobDetails)
     if (this.jobDetails) {
@@ -31,51 +35,91 @@ export class SkewComponent implements OnInit {
       this.skus = this.jobDetails.products[0].expense_skus;
       // console.log("skus", this.skus)
       this.skus.map((item, index) => {
-        let res = item.sku_measurement_details.filter(sku =>
-          sku.id == item.sku_measurement_id
-        )
+        let res = item.sku_measurement_details.filter(
+          sku => sku.id == item.sku_measurement_id
+        );
         // console.log('res', res)
         if (res) {
-          this.skuData.push({ 'id': item.id, 'sku_id': item.sku_id, 'name': item.sku_details.title, 'measurement_value': res[0].measurement_value, 'measurement_type': res[0].measurement_type.acronym, 'pack_numbers': res[0].pack_numbers, 'cashback_percent': res[0].cashback_percent, 'quantity': item.quantity, 'mrp': res[0].mrp, 'sku_measurement_id': item.sku_measurement_id, 'added_date': item.added_date });
+          this.skuData.push({
+            id: item.id,
+            sku_id: item.sku_id,
+            name: item.sku_details.title,
+            measurement_value: res[0].measurement_value,
+            measurement_type: res[0].measurement_type.acronym,
+            pack_numbers: res[0].pack_numbers,
+            cashback_percent: res[0].cashback_percent,
+            quantity: item.quantity,
+            mrp: res[0].mrp,
+            sku_measurement_id: item.sku_measurement_id,
+            added_date: item.added_date
+          });
         }
-      })
+      });
       // console.log(this.skuData)
     }
   }
   cashback(res: NgForm) {
     this.skuIds = Object.keys(res.value);
-    console.log("sku ids", this.skuIds)
+    console.log("sku ids", this.skuIds);
     let sellingPrice = [];
     let totalCashback = 0;
     let availableCashback = [];
-    console.log("response is ", res.value)
+    console.log("response is ", res.value);
     for (let i in res.value) {
       sellingPrice.push(res.value[i]);
     }
-    // calculate available_cashback 
+    // calculate available_cashback
     for (let val = 0; val < sellingPrice.length; val++) {
-      let calculate = (sellingPrice[val] * this.skuData[val].cashback_percent) / 100;
+      let calculate =
+        (sellingPrice[val] * this.skuData[val].cashback_percent) / 100;
       totalCashback += calculate;
-      availableCashback.push(calculate)
+      availableCashback.push(calculate);
     }
     for (let i = 0; i < this.skuIds.length; i++) {
-      this.skuItems.push({ 'id': this.skuIds[i], 'sku_id': this.skuData[i].sku_id, 'quantity': this.skuData[i].quantity, 'sku_measurement_id': this.skuData[i].sku_measurement_id, 'added_date': this.skuData[i].added_date, 'selling_price': sellingPrice[i], 'available_cashback': availableCashback[i] })
+      this.skuItems.push({
+        id: this.skuIds[i],
+        sku_id: this.skuData[i].sku_id,
+        quantity: this.skuData[i].quantity,
+        sku_measurement_id: this.skuData[i].sku_measurement_id,
+        added_date: this.skuData[i].added_date,
+        selling_price: sellingPrice[i],
+        available_cashback: availableCashback[i]
+      });
     }
     console.log("sku items for update", this.skuItems);
-    this.__userService.updateSKU({ 'seller_id': this.sellerId, 'cashback_value': totalCashback, 'sku_items': this.skuItems }, this.jobId)
-      .subscribe((res) => {
-        console.log("success after update", res);
-        this.__ngxNotificationService.sendMessage('SKU Verified', 'dark', 'bottom-right');
-        this.__router.navigateByUrl('/dashboard/new');
-      }, (err) => {
-        console.log("error", err)
-        this.skuItems = [];
-        let msg;
-        if (err.status == 0) {
-          msg = 'No Internet available.'
+    this.__userService
+      .updateSKU(
+        {
+          seller_id: this.sellerId,
+          cashback_value: totalCashback,
+          sku_items: this.skuItems
+        },
+        this.jobId
+      )
+      .subscribe(
+        res => {
+          console.log("success after update", res);
+          this.__ngxNotificationService.sendMessage(
+            "SKU Verified",
+            "dark",
+            "bottom-right"
+          );
+          this.__router.navigateByUrl("/dashboard/new");
+        },
+        err => {
+          console.log("error", err);
+          this.skuItems = [];
+          let msg;
+          if (err.status == 0) {
+            msg = "No Internet available.";
+          }
+          msg = err.error.reason;
+          this.__ngxNotificationService.sendMessage(
+            msg,
+            "dark",
+            "bottom-right"
+          );
         }
-        msg = err.error.reason;
-        this.__ngxNotificationService.sendMessage(msg, 'dark', 'bottom-right');
-      })
+      );
   }
 }
